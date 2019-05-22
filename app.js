b7f2667de64d5f80d99ecc6d94d2b0a89da7ada9
Chinmay2700 <50478649+Chinmay2700@users.noreply.github.com>
2019-05-22 22:51:10 +0530

    22/05

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
- 
+var formidable = require('formidable')
 // New Code
 var monk = require('monk');
 var db = monk('localhost:27017/kyc');
@@ -12,7 +12,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+var fileUpload = require('express-fileupload');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(fileUpload());
 // Make our db accessible to our router
 app.use(function(req,res,next){
 req.db = db;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'); 
+var mongo=require('mongodb')
+var router = express();
+var formidable = require('formidable')
+//var formidable = require('formidable');
+var fs = require('fs');
 console.log("Server started at port no. 3000");
+var upload=require('express-fileupload');
+router.use(upload());
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+var select;
 router.post('/upload', function(req,res) {
-    var select=req.body.selectpicker;
+    select=req.body.selectpicker;
     if(select==0)
         res.render("upload",{title: 'Aadhar', hid:false});
     else if(select==1)
@@ -18,6 +25,64 @@ router.post('/upload', function(req,res) {
 
 });
 router.post('/selfie',function(req,res){
-    res.render("video");
+    //console.log(req.body.front)
+    //console.log(req.files.front);
+    var db=req.db;
+    var id=req.body.id;
+    var image1=req.files;
+    if(select==0)
+    {
+        var image2=req.files;
+        var aadhar_coll = db.get('aadhar_coll');
+        aadhar_coll.insert({"_id":new mongo.ObjectID(id), "id":id, "front_img":image1, "back_img":image2}, function(err,doc){
+            if(err)
+                res.send("Id already exist!");
+            else
+                res.render("video");
+        });
+    }
+    else if(select==1)
+    {
+        var pan_coll = db.get('pan_coll');
+        pan_coll.insert({"_id":new mongo.ObjectID(id), "id":id, "front_img":image1}, function(err,doc){
+            if(err)
+                res.send("Id already exist!");
+            else
+                res.render("video");
+        });
+    }
+    else if(select==2)
+    {
+        var dl_coll = db.get('dl_coll');
+        dl_coll.insert({"_id":new mongo.ObjectID(id), "id":id, "front_img":image1}, function(err,doc){
+            if(err)
+                res.send("Id already exist!");
+            else
+                res.render("video");
+        });
+    }
+    else if(select==3)
+    {
+        var passport_coll = db.get('passport_coll');
+        passport_coll.insert({"_id":new mongo.ObjectID(id), "id":id, "front_img":image1}, function(err,doc){
+            if(err)
+                res.send("Id already exist!");
+            else
+                res.render("video");
+        });
+    }
+});
+router.get('/userlist',function(req,res){
+    var db=req.db;
+    var collection=db.get('aadhar_coll');
+    collection.find({},{},function(err,doc){
+        if(err)
+            res.send("There was a problem accesiing the data");
+        else
+        {
+            res.render("userlist",{"userlist":doc});
+            console.log("database successfully accessed!");
+        }
+    });
 });
 module.exports = router;
